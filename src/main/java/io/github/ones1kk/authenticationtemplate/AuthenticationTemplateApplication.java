f5cd2826bf0d72ff5716a0a5380269d32b5b1458
ones1kk <ones1k95@gmail.com>
2022-10-31 22:29:44 +0900

    Add : @EnableWebSecurity

--- src/main/java/io/github/ones1kk/authenticationtemplate/AuthenticationTemplateApplication.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/AuthenticationTemplateApplication.java
@@ -2,8 +2,10 @@ package io.github.ones1kk.authenticationtemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class AuthenticationTemplateApplication {
 
     public static void main(String[] args) {
