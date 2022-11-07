5c02afd2a2661d3084f438387027d5dc87f5f46c
ones1kk <ones1k95@gmail.com>
2022-11-07 23:29:31 +0900

    Update : InitData > Label, Message

--- src/main/java/io/github/ones1kk/authenticationtemplate/InitData.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/InitData.java
@@ -1,12 +1,18 @@
 package io.github.ones1kk.authenticationtemplate;
 
+import io.github.ones1kk.authenticationtemplate.domain.Label;
+import io.github.ones1kk.authenticationtemplate.domain.Message;
 import io.github.ones1kk.authenticationtemplate.domain.User;
+import io.github.ones1kk.authenticationtemplate.repository.LabelRepository;
+import io.github.ones1kk.authenticationtemplate.repository.MessageRepository;
 import io.github.ones1kk.authenticationtemplate.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -27,9 +33,23 @@ public class InitData {
 
         private final UserRepository userRepository;
 
+        private final LabelRepository labelRepository;
+
+        private final MessageRepository messageRepository;
+
         public void dbInit() {
             User user = new User("ones1kk", "$2a$10$nwOTfZvyYwFlIDbrO/496eYGf3VaLup0htWeX/UH7IweIN/tsc5dO");
             userRepository.save(user);
+
+            List<Label> labels = new ArrayList<>();
+            List<Message> messages = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                labels.add(new Label("L" + i, "korean", "english"));
+                messages.add(new Message("M" + i, "korean", "english"));
+            }
+            labelRepository.saveAll(labels);
+            messageRepository.saveAll(messages);
         }
     }
 }
