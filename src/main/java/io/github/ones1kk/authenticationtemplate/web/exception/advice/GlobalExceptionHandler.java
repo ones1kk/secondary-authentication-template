6b12d7dc44869670dbf002d6ef4ec81ebd69e957
ones1kk <ones1k95@gmail.com>
2022-11-09 00:07:27 +0900

    Add : GlobalExceptionHandling

--- src/main/java/io/github/ones1kk/authenticationtemplate/web/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/web/exception/advice/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package io.github.ones1kk.authenticationtemplate.web.exception.advice;
 
+import io.github.ones1kk.authenticationtemplate.web.exception.MessageSupport;
 import io.github.ones1kk.authenticationtemplate.web.exception.model.ExceptionResponse;
 import io.github.ones1kk.authenticationtemplate.web.exception.model.GlobalException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    private final MessageSupport messageSupport;
+
     @ExceptionHandler(GlobalException.class)
-    public ExceptionResponse handle(GlobalException e, HttpServletResponse response) {
-        String message = e.getMessage();
+    ExceptionResponse handle(GlobalException e, HttpServletResponse response) {
+        String message = messageSupport.get(e.getCode(), e.getArgs());
         response.setStatus(e.getStatus().value());
         return new ExceptionResponse(response.getStatus(), message);
     }
--- src/main/java/io/github/ones1kk/authenticationtemplate/web/exception/model/GlobalException.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/web/exception/model/GlobalException.java
@@ -2,25 +2,24 @@ package io.github.ones1kk.authenticationtemplate.web.exception.model;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
 
 @Getter
-public class GlobalException extends AuthenticationException {
+public class GlobalException extends RuntimeException {
 
-    private final String message;
+    private final String code;
 
     private final HttpStatus status;
 
     private final Object[] args;
 
-    public GlobalException(String message, Object... args) {
-        this(message, HttpStatus.INTERNAL_SERVER_ERROR, args);
+    public GlobalException(String code, Object... args) {
+        this(code, HttpStatus.INTERNAL_SERVER_ERROR, args);
     }
 
-    public GlobalException(String message, HttpStatus status, Object... args) {
-        super(message);
+    public GlobalException(String code, HttpStatus status, Object... args) {
+        super(code);
         this.status = status;
-        this.message = message;
+        this.code = code;
         this.args = args;
     }
 }
