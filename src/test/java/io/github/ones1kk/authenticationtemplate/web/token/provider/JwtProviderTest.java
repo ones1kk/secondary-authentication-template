0e9febd771567459a87490234fbfef5b8792816d
ones1kk <ones1k95@gmail.com>
2022-11-12 00:44:42 +0900

    Update : JwtProviderTest

--- src/test/java/io/github/ones1kk/authenticationtemplate/web/token/provider/JwtProviderTest.java
+++ src/test/java/io/github/ones1kk/authenticationtemplate/web/token/provider/JwtProviderTest.java
@@ -1,12 +1,15 @@
 package io.github.ones1kk.authenticationtemplate.web.token.provider;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JwtProviderTest {
     private static final String SECRET_KEY = "c88d74ba-1554-48a4-b549-b926f5d77c9e";
@@ -17,30 +20,51 @@ class JwtProviderTest {
     void createToken() throws Exception {
         // given
         List<String> tokens = new ArrayList<>();
-
         // when
         for (long i = 0; i < 10; i++) {
             tokens.add(jwtProvider.createToken(i));
         }
+        boolean isUnique = tokens.stream()
+                .allMatch(new HashSet<>()::add);
+
 
         // then
         assertThat(tokens).extracting(String::length)
                 .allMatch(length -> length.equals(135));
-        assertThat(new HashSet<>(tokens).size()).isEqualTo(tokens.size());
+        assertThat(isUnique).isTrue();
     }
 
-    @Test
-    void isExpired() throws Exception {
-        // given
+    @Nested
+    @DisplayName("verify expired time of access token ")
+    class JWTExpiredTest {
         final Long id = 1L;
 
-        // when
-        String token = jwtProvider.createToken(id);
-        Thread.sleep(2 * 90 * 60 * 1000L);
-        boolean expired = jwtProvider.isExpired(token);
+        @Test
+        @DisplayName("request after expired time")
+        void isExpired_true() throws Exception {
+            // given
+            String token = jwtProvider.createToken(id, 1000L);
 
-        // then
-        System.out.println(expired);
+            // when
+            Thread.sleep(1500L);
+
+            // then
+            assertThatThrownBy(() -> jwtProvider.isExpired(token))
+                    .isInstanceOf(SecurityException.class);
+        }
+
+        @Test
+        @DisplayName("request before expired time")
+        void isExpired_false() throws Exception {
+            // given
+            String token = jwtProvider.createToken(id, 1000L);
+
+            // when
+            Thread.sleep(200L);
+
+            // then
+            assertThat(jwtProvider.isExpired(token)).isFalse();
+        }
     }
 
 }
