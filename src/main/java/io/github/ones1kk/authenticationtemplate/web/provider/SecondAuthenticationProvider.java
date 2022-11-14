1f6c22262694182a3ecb202083ee1208c00ce6b6
ones1kk <ones1k95@gmail.com>
2022-11-15 00:12:36 +0900

    Update : AuthenticationProvider

--- src/main/java/io/github/ones1kk/authenticationtemplate/web/provider/SecondAuthenticationProvider.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/web/provider/SecondAuthenticationProvider.java
@@ -3,15 +3,25 @@ package io.github.ones1kk.authenticationtemplate.web.provider;
 import io.github.ones1kk.authenticationtemplate.web.dto.SecondLoginDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 @RequiredArgsConstructor
 public class SecondAuthenticationProvider implements AuthenticationProvider {
 
+    private static final String ANSWER = "123456";
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         SecondLoginDto secondLoginDto = (SecondLoginDto) authentication.getPrincipal();
+        String authenticationNumber = secondLoginDto.getAuthenticationNumber();
+
+        if (!ANSWER.equals(authenticationNumber)) {
+            throw new BadCredentialsException("M3");
+        }
+
+//        Authentication token = new SecondAuthenticationToken();
 
         return null;
     }
--- src/main/java/io/github/ones1kk/authenticationtemplate/web/provider/hanlder/FirstAuthenticationSuccessHandler.java
+++ src/main/java/io/github/ones1kk/authenticationtemplate/web/provider/hanlder/FirstAuthenticationSuccessHandler.java
@@ -2,6 +2,8 @@ package io.github.ones1kk.authenticationtemplate.web.provider.hanlder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.ones1kk.authenticationtemplate.web.exception.MessageSupport;
+import io.github.ones1kk.authenticationtemplate.web.token.FirstAuthenticationToken;
+import io.github.ones1kk.authenticationtemplate.web.token.provider.JwtProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -10,14 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static io.github.ones1kk.authenticationtemplate.web.token.provider.constant.TokenHeaderName.X_AUTH_TOKEN;
+
 public class FirstAuthenticationSuccessHandler extends AbstractAuthenticationHandler implements AuthenticationSuccessHandler {
 
-    public FirstAuthenticationSuccessHandler(ObjectMapper objectMapper, MessageSupport messageSupport) {
+    private final JwtProvider<Authentication> jwtProvider;
+
+    public FirstAuthenticationSuccessHandler(ObjectMapper objectMapper, MessageSupport messageSupport, JwtProvider<Authentication> jwtProvider) {
         super(objectMapper, messageSupport);
+        this.jwtProvider = jwtProvider;
     }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // TODO create token
+        Long principal = (Long) authentication.getPrincipal();
+        Authentication token = new FirstAuthenticationToken(principal);
+        String accessToken = jwtProvider.createAccessToken(token);
+        token.setAuthenticated(true);
+
+        response.setHeader(X_AUTH_TOKEN.getName(), accessToken);
     }
 }
