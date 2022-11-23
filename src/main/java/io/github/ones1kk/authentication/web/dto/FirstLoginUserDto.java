fc917b97d4a9ceb51ee604389f873d3d6a10d974
ones1kk <ones1k95@gmail.com>
2022-11-23 23:06:07 +0900

    Add : Construct

--- src/main/java/io/github/ones1kk/authentication/web/dto/FirstLoginUserDto.java
+++ src/main/java/io/github/ones1kk/authentication/web/dto/FirstLoginUserDto.java
@@ -1,9 +1,13 @@
 package io.github.ones1kk.authentication.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class FirstLoginUserDto {
     private String id;
--- src/main/java/io/github/ones1kk/authentication/web/dto/SecondLoginDto.java
+++ src/main/java/io/github/ones1kk/authentication/web/dto/SecondLoginDto.java
@@ -1,8 +1,12 @@
 package io.github.ones1kk.authentication.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SecondLoginDto {
     private String authenticationNumber;
 }
